f4ec9e2ae1b93221d4fd3bdee87d1399a6992697
HuseyinSoyturk <36479738+HuseyinSoyturk@users.noreply.github.com>
2020-03-13 10:44:51 +0300

    lib added

---
+++ lib/interface/IGeoJson.d.ts
@@ -0,0 +1,12 @@
+import { IGeometry } from "./IGeometry";
+export interface IGeoJson {
+    type: string;
+    geometry: IGeometry;
+    properties: ICustomObject;
+    id?: string;
+    geometry_name?: string;
+}
+interface ICustomObject {
+    [key: string]: any;
+}
+export {};
---
+++ lib/interface/IGeometry.d.ts
@@ -0,0 +1,4 @@
+export interface IGeometry {
+    type: 'Point' | 'LineString' | 'Polygon' | 'MultiPoint' | 'MultiLineString' | 'MultiPolygon';
+    coordinates: number[] | number[][];
+}
