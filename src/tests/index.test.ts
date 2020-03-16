19388bc10f8d7a929d1d8caddc50865e7eccea48
HuseyinSoyturk <36479738+HuseyinSoyturk@users.noreply.github.com>
2020-03-16 16:10:38 +0300

    1.1.4

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,6 +1,6 @@
 import Digitization from '../index';
 
 test('sum must be right', () => {
-  const digi = new Digitization({workspace:'sadasdsa' , url:'sadsadasdas'});
+  const digi = new Digitization({ workspace: 'sadasdsa', url: 'sadsadasdas' });
   expect(digi.sum(2, 5)).toBe(7);
 });
