8b0e284a19925e88a65fe6f49c640b6a2551d58c
HuseyinSoyturk <36479738+HuseyinSoyturk@users.noreply.github.com>
2020-03-18 11:48:27 +0300

    lint changes

--- src/index.ts
+++ src/index.ts
@@ -321,7 +321,7 @@ class Digitization {
       method: 'post',
       body: xml,
       headers: { 'Content-Type': 'text/xml' },
-    }).then(res => console.warn(res));
+    }).then(res => console.log(res));
   }
 }
 
